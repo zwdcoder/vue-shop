d88e330b23a34affcfad182b1f4d66a760820423
zwdcoder <1448933153@qq.com>
2021-01-25 12:22:51 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   chainWebpack: config => {
+    //发布模式
     config.when(process.env.NODE_ENV === 'production', config => {
       config.entry('app').clear().add('./src/main-prod.js')
     })
+
+    //开发模式
     config.when(process.env.NODE_ENV === 'development', config => {
       config.entry('app').clear().add('./src/main-dev.js')
     })
